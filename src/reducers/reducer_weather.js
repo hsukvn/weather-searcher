34b4ece98b92b9237598d2ecc7e688c0161b9504
kevinhsu <kevinhsu@synology.com>
2017-03-28 00:07:26 +0800

    Add google map and chart

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,6 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function (state = [], action) {
   switch(action.type) {
   case FETCH_WEATHER:
+    // Never change the original state. Create new one instead.
     return [ action.payload.data, ...state ]; //state.concate([action.payload.data])
   }
 
